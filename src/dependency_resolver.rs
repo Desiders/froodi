c1a087d8e4fb91a0290d89a62c835b6eaa286b59
Desiders <abc_desi@proton.me>
2025-04-21 20:08:31 +0300

    Add `InjectTransient` and move caching in dependency resolve from instantiator

--- src/dependency_resolver.rs
+++ src/dependency_resolver.rs
@@ -3,7 +3,7 @@ use core::{
     any::{type_name, TypeId},
     cell::RefCell,
 };
-use tracing::{debug_span, error, warn};
+use tracing::{debug, debug_span, error, warn};
 
 use super::{
     context::Context,
@@ -19,9 +19,61 @@ pub(crate) trait DependencyResolver: Sized {
     fn resolve(registry: Rc<Registry>, context: Rc<RefCell<Context>>) -> Result<Self, Self::Error>;
 }
 
-pub(crate) struct Inject<Dep>(pub(crate) Dep);
+pub(crate) struct Inject<Dep>(pub(crate) Rc<Dep>);
 
 impl<Dep: 'static> DependencyResolver for Inject<Dep> {
+    type Error = ResolveErrorKind;
+
+    fn resolve(registry: Rc<Registry>, context: Rc<RefCell<Context>>) -> Result<Self, Self::Error> {
+        let span = debug_span!("resolve", dependency = type_name::<Dep>());
+        let _guard = span.enter();
+
+        if let Some(dependency) = context.borrow().get::<Dep>() {
+            debug!("Found in context");
+            return Ok(Self(dependency));
+        }
+        debug!("Not found in context");
+
+        let Some((mut instantiator, config)) = registry.get_instantiator::<Dep>() else {
+            let err = ResolveErrorKind::NoInstantiator;
+            warn!("{}", err);
+            return Err(err);
+        };
+
+        match instantiator.call(Request::new(registry, config, context.clone())) {
+            Ok(dependency) => match dependency.downcast::<Dep>() {
+                Ok(dependency) => {
+                    let dependency = Rc::new(*dependency);
+                    if config.cache_provides {
+                        context.borrow_mut().insert_rc(dependency.clone());
+                        debug!("Cached");
+                    }
+                    Ok(Self(dependency))
+                }
+                Err(incorrect_type) => {
+                    let err = ResolveErrorKind::IncorrectType {
+                        expected: TypeId::of::<Dep>(),
+                        actual: incorrect_type.type_id(),
+                    };
+                    error!("{}", err);
+                    Err(err)
+                }
+            },
+            Err(InstantiatorErrorKind::Deps(err)) => {
+                error!("{}", err);
+                Err(ResolveErrorKind::Instantiator(InstantiatorErrorKind::Deps(Box::new(err))))
+            }
+            Err(InstantiatorErrorKind::Factory(err)) => {
+                error!("{}", err);
+                Err(ResolveErrorKind::Instantiator(InstantiatorErrorKind::Factory(err)))
+            }
+        }
+    }
+}
+
+pub(crate) struct InjectTransient<Dep>(pub(crate) Dep);
+
+impl<Dep: 'static> DependencyResolver for InjectTransient<Dep> {
     type Error = ResolveErrorKind;
 
     fn resolve(registry: Rc<Registry>, context: Rc<RefCell<Context>>) -> Result<Self, Self::Error> {
@@ -34,29 +86,27 @@ impl<Dep: 'static> DependencyResolver for Inject<Dep> {
             return Err(err);
         };
 
-        let dependency = match instantiator.call(Request::new(registry, config, context)) {
+        match instantiator.call(Request::new(registry, config, context.clone())) {
             Ok(dependency) => match dependency.downcast::<Dep>() {
-                Ok(dependency) => *dependency,
+                Ok(dependency) => Ok(Self(*dependency as _)),
                 Err(incorrect_type) => {
                     let err = ResolveErrorKind::IncorrectType {
                         expected: TypeId::of::<Dep>(),
                         actual: incorrect_type.type_id(),
                     };
                     error!("{}", err);
-                    return Err(err);
+                    Err(err)
                 }
             },
             Err(InstantiatorErrorKind::Deps(err)) => {
                 error!("{}", err);
-                return Err(ResolveErrorKind::Instantiator(InstantiatorErrorKind::Deps(Box::new(err))));
+                Err(ResolveErrorKind::Instantiator(InstantiatorErrorKind::Deps(Box::new(err))))
             }
             Err(InstantiatorErrorKind::Factory(err)) => {
                 error!("{}", err);
-                return Err(ResolveErrorKind::Instantiator(InstantiatorErrorKind::Factory(err)));
+                Err(ResolveErrorKind::Instantiator(InstantiatorErrorKind::Factory(err)))
             }
-        };
-
-        Ok(Self(dependency))
+        }
     }
 }
 
