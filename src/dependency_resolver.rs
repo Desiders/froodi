096aeec6f8af1eafb89245133366f8b5fabc757e
Desiders <abc_desi@proton.me>
2025-05-04 04:15:02 +0300

    Add dependency resolver tests

--- src/dependency_resolver.rs
+++ src/dependency_resolver.rs
@@ -131,3 +131,87 @@ macro_rules! impl_dependency_resolver {
 }
 
 all_the_tuples!(impl_dependency_resolver);
+
+#[cfg(test)]
+mod tests {
+    extern crate std;
+
+    use alloc::{
+        format,
+        rc::Rc,
+        string::{String, ToString as _},
+    };
+    use core::{
+        cell::RefCell,
+        sync::atomic::{AtomicU8, Ordering},
+    };
+    use tracing::debug;
+    use tracing_test::traced_test;
+
+    use super::{Context, DependencyResolver, Inject, InjectTransient, Registry};
+    use crate::{errors::InstantiateErrorKind, instantiator::boxed_instantiator_factory};
+
+    struct Request;
+
+    #[test]
+    fn test_dependency_resolver_impls() {
+        fn resolver<T: DependencyResolver>() {}
+        fn resolver_with_dep<Dep: 'static>() {
+            resolver::<Inject<Dep>>();
+            resolver::<InjectTransient<Dep>>();
+
+            resolver::<(Inject<Dep>, InjectTransient<Dep>)>();
+        }
+    }
+
+    #[test]
+    #[traced_test]
+    fn test_scoped_resolve() {
+        let instantiator_request_call_count = Rc::new(AtomicU8::new(0));
+
+        let mut registry = Registry::default();
+        registry.add_instantiator::<Request>(boxed_instantiator_factory({
+            let instantiator_request_call_count = instantiator_request_call_count.clone();
+            move || {
+                instantiator_request_call_count.fetch_add(1, Ordering::SeqCst);
+
+                debug!("Call instantiator request");
+                Ok::<_, InstantiateErrorKind>(Request)
+            }
+        }));
+
+        let registry = Rc::new(registry);
+        let context = Rc::new(RefCell::new(Context::default()));
+
+        let request_1 = Inject::<Request>::resolve(registry.clone(), context.clone()).unwrap();
+        let request_2 = Inject::<Request>::resolve(registry, context).unwrap();
+
+        assert!(Rc::ptr_eq(&request_1.0, &request_2.0));
+        assert_eq!(instantiator_request_call_count.load(Ordering::SeqCst), 1);
+    }
+
+    #[test]
+    #[traced_test]
+    fn test_transient_resolve() {
+        let instantiator_request_call_count = Rc::new(AtomicU8::new(0));
+
+        let mut registry = Registry::default();
+        registry.add_instantiator::<Request>(boxed_instantiator_factory({
+            let instantiator_request_call_count = instantiator_request_call_count.clone();
+            move || {
+                instantiator_request_call_count.fetch_add(1, Ordering::SeqCst);
+
+                debug!("Call instantiator request");
+                Ok::<_, InstantiateErrorKind>(Request)
+            }
+        }));
+
+        let registry = Rc::new(registry);
+        let context = Rc::new(RefCell::new(Context::default()));
+
+        InjectTransient::<Request>::resolve(registry.clone(), context.clone()).unwrap();
+        InjectTransient::<Request>::resolve(registry, context).unwrap();
+
+        assert_eq!(instantiator_request_call_count.load(Ordering::SeqCst), 2);
+    }
+}
