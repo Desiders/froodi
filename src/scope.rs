d2ca4b52371e04fc6bccf8b028e781f93f128e90
Desiders <47452083+Desiders@users.noreply.github.com>
2025-07-11 04:31:06 +0300

    Add scope

---
+++ src/scope.rs
@@ -0,0 +1,47 @@
+pub trait Scope: Ord {
+    #[must_use]
+    fn name(&self) -> &'static str;
+
+    #[must_use]
+    fn priority(&self) -> u8;
+
+    #[must_use]
+    fn is_skipped_by_default(&self) -> bool {
+        false
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(u8)]
+pub enum DefaultScope {
+    Runtime,
+    App,
+    Session,
+    Request,
+    Action,
+    Step,
+}
+
+impl Scope for DefaultScope {
+    #[inline]
+    fn name(&self) -> &'static str {
+        match self {
+            DefaultScope::Runtime => "runtime",
+            DefaultScope::App => "app",
+            DefaultScope::Session => "session",
+            DefaultScope::Request => "request",
+            DefaultScope::Action => "action",
+            DefaultScope::Step => "step",
+        }
+    }
+
+    #[inline]
+    fn priority(&self) -> u8 {
+        *self as u8
+    }
+
+    #[inline]
+    fn is_skipped_by_default(&self) -> bool {
+        matches!(self, DefaultScope::Runtime | DefaultScope::Session)
+    }
+}
