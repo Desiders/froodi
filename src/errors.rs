893986230b75b7de72d199890cd9d3cb45f2a1d9
Desiders <47452083+Desiders@users.noreply.github.com>
2025-07-11 04:31:54 +0300

    Add container scope errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,9 @@
+mod container;
 mod dependency_resolver;
 mod instantiate;
 mod instantiator;
 
+pub use container::{ScopeErrorKind, ScopeWithErrorKind};
 pub use dependency_resolver::ResolveErrorKind;
 pub use instantiate::InstantiateErrorKind;
 pub use instantiator::InstantiatorErrorKind;
