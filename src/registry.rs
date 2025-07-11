03b382899d042c3530f1acf253468d723fd5bb94
Desiders <47452083+Desiders@users.noreply.github.com>
2025-07-11 04:35:26 +0300

    Add registry builder and refactor types usage

--- src/registry.rs
+++ src/registry.rs
@@ -1,4 +1,4 @@
-use alloc::collections::BTreeMap;
+use alloc::{collections::BTreeMap, vec, vec::Vec};
 use core::any::TypeId;
 
 use super::{
@@ -8,14 +8,20 @@ use super::{
 use crate::{
     dependency_resolver::DependencyResolver,
     instantiator::{boxed_instantiator_factory, Instantiator},
+    scope::Scope,
 };
 
-#[derive(Default, Clone)]
-pub struct Registry {
-    instantiators: BTreeMap<TypeId, (BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>, Config)>,
+pub(crate) struct InstantiatorData<S> {
+    instantiator: BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>,
+    config: Config,
+    scope: S,
 }
 
-impl Registry {
+pub struct RegistriesBuilder<S> {
+    instantiators: BTreeMap<TypeId, InstantiatorData<S>>,
+}
+
+impl<S> RegistriesBuilder<S> {
     #[inline]
     #[must_use]
     pub const fn new() -> Self {
@@ -27,53 +33,172 @@ impl Registry {
     #[inline]
     #[allow(private_bounds)]
     #[must_use]
-    pub fn provide<Inst, Deps>(mut self, instantiator: Inst) -> Self
+    pub fn provide<Inst, Deps>(mut self, instantiator: Inst, scope: S) -> Self
     where
         Inst: Instantiator<Deps, Error = InstantiateErrorKind>,
         Deps: DependencyResolver<Error = ResolveErrorKind>,
     {
-        self.add_instantiator::<Inst::Provides>(boxed_instantiator_factory(instantiator));
+        self.add_instantiator::<Inst::Provides>(boxed_instantiator_factory(instantiator), scope);
         self
     }
 
     #[inline]
     #[allow(private_bounds)]
     #[must_use]
-    pub fn provide_with_config<Inst, Deps>(mut self, instantiator: Inst, config: Config) -> Self
+    pub fn provide_with_config<Inst, Deps>(mut self, instantiator: Inst, config: Config, scope: S) -> Self
     where
         Inst: Instantiator<Deps, Error = InstantiateErrorKind>,
         Deps: DependencyResolver<Error = ResolveErrorKind>,
     {
-        self.add_instantiator_with_config::<Inst::Provides>(boxed_instantiator_factory(instantiator), config);
+        self.add_instantiator_with_config::<Inst::Provides>(boxed_instantiator_factory(instantiator), config, scope);
         self
     }
 }
 
-impl Registry {
+impl<S> RegistriesBuilder<S> {
     pub(crate) fn add_instantiator<Dep: 'static>(
         &mut self,
-        value: BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>,
-    ) -> Option<(BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>, Config)> {
-        self.instantiators.insert(TypeId::of::<Dep>(), (value, Config::default()))
+        instantiator: BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>,
+        scope: S,
+    ) -> Option<InstantiatorData<S>> {
+        self.instantiators.insert(
+            TypeId::of::<Dep>(),
+            InstantiatorData {
+                instantiator,
+                scope,
+                config: Config::default(),
+            },
+        )
     }
 
     pub(crate) fn add_instantiator_with_config<Dep: 'static>(
         &mut self,
-        value: BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>,
+        instantiator: BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>,
         config: Config,
-    ) -> Option<(BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>, Config)> {
-        self.instantiators.insert(TypeId::of::<Dep>(), (value, config))
+        scope: S,
+    ) -> Option<InstantiatorData<S>> {
+        self.instantiators.insert(
+            TypeId::of::<Dep>(),
+            InstantiatorData {
+                instantiator,
+                config,
+                scope,
+            },
+        )
     }
+}
 
+impl<S> RegistriesBuilder<S>
+where
+    S: Scope,
+{
+    pub(crate) fn build(self) -> Vec<Registry> {
+        use alloc::collections::btree_map::Entry::*;
+
+        let mut scopes_instantiators: BTreeMap<S, Vec<(TypeId, InstantiatorInnerData)>> = BTreeMap::new();
+        for (
+            type_id,
+            InstantiatorData {
+                instantiator,
+                config,
+                scope,
+            },
+        ) in self.instantiators
+        {
+            match scopes_instantiators.entry(scope) {
+                Vacant(entry) => {
+                    entry.insert(vec![(type_id, InstantiatorInnerData { instantiator, config })]);
+                }
+                Occupied(entry) => {
+                    entry.into_mut().push((type_id, InstantiatorInnerData { instantiator, config }));
+                }
+            }
+        }
+
+        let mut registries = Vec::with_capacity(scopes_instantiators.len());
+        for (scope, instantiators) in scopes_instantiators {
+            registries.push(Registry {
+                scope: ScopeInnerData {
+                    priority: scope.priority(),
+                    is_skipped_by_default: scope.is_skipped_by_default(),
+                },
+                instantiators: BTreeMap::from_iter(instantiators),
+            });
+        }
+
+        registries
+    }
+}
+
+#[cfg_attr(feature = "debug", derive(Debug))]
+pub(crate) struct ScopeInnerData {
+    pub(crate) priority: u8,
+    pub(crate) is_skipped_by_default: bool,
+}
+
+#[derive(Clone)]
+#[cfg_attr(feature = "debug", derive(Debug))]
+pub(crate) struct InstantiatorInnerData {
+    pub(crate) instantiator: BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>,
+    pub(crate) config: Config,
+}
+
+#[cfg_attr(feature = "debug", derive(Debug))]
+pub(crate) struct Registry {
+    pub(crate) scope: ScopeInnerData,
+    instantiators: BTreeMap<TypeId, InstantiatorInnerData>,
+}
+
+impl Registry {
     #[must_use]
     pub(crate) fn get_instantiator<Dep: 'static>(&self) -> Option<BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>> {
-        self.instantiators.get(&TypeId::of::<Dep>()).map(|(value, _)| value.clone())
+        self.instantiators.get(&TypeId::of::<Dep>()).map(|data| data.instantiator.clone())
     }
 
     #[must_use]
-    pub(crate) fn get_instantiator_with_config<Dep: 'static>(
-        &self,
-    ) -> Option<(BoxedCloneInstantiator<ResolveErrorKind, InstantiateErrorKind>, Config)> {
+    pub(crate) fn get_instantiator_data<Dep: 'static>(&self) -> Option<InstantiatorInnerData> {
         self.instantiators.get(&TypeId::of::<Dep>()).cloned()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::RegistriesBuilder;
+    use crate::scope::DefaultScope::{self, *};
+
+    #[test]
+    fn test_build_empty() {
+        let registries = RegistriesBuilder::<DefaultScope>::new().build();
+        assert!(registries.is_empty());
+    }
+
+    #[test]
+    fn test_build_equal_provides() {
+        let registries = RegistriesBuilder::new()
+            .provide(|| Ok(()), Runtime)
+            .provide(|| Ok(()), Runtime)
+            .provide(|| Ok(()), App)
+            .provide(|| Ok(()), App)
+            .build();
+        assert_eq!(registries.len(), 1);
+
+        for registry in registries {
+            assert_eq!(registry.instantiators.len(), 1);
+        }
+    }
+
+    #[test]
+    fn test_build_several_scopes() {
+        let registries = RegistriesBuilder::new()
+            .provide(|| Ok(1i8), Runtime)
+            .provide(|| Ok(1i16), Runtime)
+            .provide(|| Ok(1i32), App)
+            .provide(|| Ok(1i64), App)
+            .build();
+        assert_eq!(registries.len(), 2);
+
+        for registry in registries {
+            assert_eq!(registry.instantiators.len(), 2);
+        }
     }
 }
