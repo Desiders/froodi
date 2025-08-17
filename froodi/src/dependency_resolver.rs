c0391f5ce7eef7ab6729b35d8657fb5e68cd4950
Desiders <47452083+Desiders@users.noreply.github.com>
2025-08-17 11:59:46 +0300

    Lint changes

--- froodi/src/dependency_resolver.rs
+++ froodi/src/dependency_resolver.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "async")]
 use core::future::Future;
 
 use super::errors::ResolveErrorKind;
--- froodi/src/utils/future.rs
+++ froodi/src/utils/future.rs
@@ -1,4 +1,5 @@
 use alloc::boxed::Box;
 use core::{future::Future, pin::Pin};
 
+#[allow(dead_code)]
 pub(crate) type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'a>>;
