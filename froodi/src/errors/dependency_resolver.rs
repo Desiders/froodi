54b289c15c39566ad9ea1cbaa2bfaf0024ed510f
Desiders <47452083+Desiders@users.noreply.github.com>
2025-10-12 05:21:24 +0300

    Add `NoAccessible` for `ResolveErrorKind`

--- froodi/src/errors/dependency_resolver.rs
+++ froodi/src/errors/dependency_resolver.rs
@@ -1,12 +1,27 @@
 use alloc::boxed::Box;
 use core::any::TypeId;
 
+use crate::ScopeData;
+
 use super::{instantiate::InstantiateErrorKind, instantiator::InstantiatorErrorKind};
 
 #[derive(thiserror::Error, Debug)]
 pub enum ResolveErrorKind {
     #[error("Instantiator not found in registry")]
     NoInstantiator,
+    #[error(
+        "\
+        Instantiator no accessible. \
+        You can't access the instantiator from child scope. \
+        Actual scope: {} ({} priority), expected: {} ({} priority)\
+        ",
+        actual_scope_data.name, actual_scope_data.priority,
+        expected_scope_data.name, expected_scope_data.priority,
+    )]
+    NoAccessible {
+        expected_scope_data: ScopeData,
+        actual_scope_data: ScopeData,
+    },
     #[error("Incorrect instantiator provides type. Actual: {actual:?}, expected: {expected:?}")]
     IncorrectType { expected: TypeId, actual: TypeId },
     #[error(transparent)]
