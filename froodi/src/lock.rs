f9b1dc25c587a6dfd4e0cbc3c043b2d067c801c7
Desiders <47452083+Desiders@users.noreply.github.com>
2026-06-27 14:24:39 +0300

    Remove `cleanup_unused` method

--- froodi/src/lock.rs
+++ froodi/src/lock.rs
@@ -1,59 +1,61 @@
-#![allow(dead_code)]
+#[cfg(feature = "thread_safe")]
+mod sync_impl {
+    use alloc::collections::BTreeMap;
+    use core::any::TypeId;
 
-use alloc::collections::BTreeMap;
-use core::any::TypeId;
-use parking_lot::{Mutex, RwLock};
+    use parking_lot::{Mutex, RwLock};
 
-use crate::utils::thread_safety::RcThreadSafety;
+    use crate::utils::thread_safety::RcThreadSafety;
 
-#[derive(Clone)]
-pub(crate) struct PerTypeLocks {
-    locks: RcThreadSafety<RwLock<BTreeMap<TypeId, RcThreadSafety<Mutex<()>>>>>,
-}
+    #[derive(Clone)]
+    pub(crate) struct PerTypeLocks {
+        locks: RcThreadSafety<RwLock<BTreeMap<TypeId, RcThreadSafety<Mutex<()>>>>>,
+    }
 
-impl PerTypeLocks {
-    #[inline]
-    #[must_use]
-    fn new() -> Self {
-        Self {
-            locks: RcThreadSafety::new(RwLock::new(BTreeMap::new())),
+    impl PerTypeLocks {
+        #[inline]
+        #[must_use]
+        fn new() -> Self {
+            Self {
+                locks: RcThreadSafety::new(RwLock::new(BTreeMap::new())),
+            }
+        }
+
+        #[inline]
+        #[must_use]
+        pub(crate) fn get(&self, type_id: TypeId) -> RcThreadSafety<Mutex<()>> {
+            if let Some(lock) = self.locks.read().get(&type_id) {
+                return lock.clone();
+            }
+
+            self.locks
+                .write()
+                .entry(type_id)
+                .or_insert_with(|| RcThreadSafety::new(Mutex::new(())))
+                .clone()
+        }
+    }
+
+    impl Default for PerTypeLocks {
+        #[inline]
+        fn default() -> Self {
+            Self::new()
         }
     }
 }
 
-impl PerTypeLocks {
-    #[inline]
-    #[must_use]
-    pub(crate) fn get(&self, type_id: TypeId) -> RcThreadSafety<Mutex<()>> {
-        if let Some(lock) = self.locks.read().get(&type_id) {
-            return lock.clone();
-        }
-
-        self.locks
-            .write()
-            .entry(type_id)
-            .or_insert_with(|| RcThreadSafety::new(Mutex::new(())))
-            .clone()
-    }
-
-    #[inline]
-    pub(crate) fn cleanup_unused(&self) {
-        self.locks.write().retain(|_, lock| RcThreadSafety::strong_count(lock) > 1);
-    }
-}
-
-impl Default for PerTypeLocks {
-    #[inline]
-    fn default() -> Self {
-        Self::new()
-    }
-}
+#[cfg(feature = "thread_safe")]
+pub(crate) use sync_impl::PerTypeLocks;
 
 #[cfg(feature = "async")]
 mod async_impl {
+    use alloc::collections::BTreeMap;
+    use core::any::TypeId;
+
+    use parking_lot::RwLock;
     use tokio::sync::Mutex;
 
-    use super::{BTreeMap, RcThreadSafety, RwLock, TypeId};
+    use crate::utils::thread_safety::RcThreadSafety;
 
     #[derive(Clone)]
     pub(crate) struct PerTypeSharedLocks {
@@ -68,9 +70,7 @@ mod async_impl {
                 locks: RcThreadSafety::new(RwLock::new(BTreeMap::new())),
             }
         }
-    }
 
-    impl PerTypeSharedLocks {
         #[inline]
         #[must_use]
         pub(crate) fn get(&self, type_id: TypeId) -> RcThreadSafety<Mutex<()>> {
@@ -83,11 +83,6 @@ mod async_impl {
                 .entry(type_id)
                 .or_insert_with(|| RcThreadSafety::new(Mutex::new(())))
                 .clone()
-        }
-
-        #[inline]
-        pub(crate) fn cleanup_unused(&self) {
-            self.locks.write().retain(|_, lock| RcThreadSafety::strong_count(lock) > 1);
         }
     }
 
